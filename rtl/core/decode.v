51a6249f22e7a0fe66d9b931e564d188512332e4
xiachunqiudong <xiadaoliang@outlook.com>
2022-11-26 22:03:45 +0800

    2022/11/26

--- rtl/core/decode.v
+++ rtl/core/decode.v
@@ -1,14 +1,38 @@
 `include "defines.v"
 
+// INSTRUCTION FETCH
+
 module decode(
-    input  [`INSTR_SIZE-1:0] instr_i,
-    output [`REG_IDX_WIDTH-1:0] dec_rs1_idx_o, 
-    output [`REG_IDX_WIDTH-1:0] dec_rs1_idx_o, 
+    input  [`INSTR_SIZE-1:0]    instr_i,
+
+    output [`REG_IDX_WIDTH-1:0] dec_rs1_idx_o,
+    output [`REG_IDX_WIDTH-1:0] dec_rs2_idx_o, 
     output [`REG_IDX_WIDTH-1:0] dec_rd_idx_o,
-    output dec_rs1_en,
-    output dec_rs2_en,
-    output dec_rd_en
+    output                      dec_rs1_en,
+    output                      dec_rs2_en,
+    output                      dec_rd_en,
+    
+    input [`XLEN-1:0]           dec_rs1_i,
+    input [`XLEN-1:0]           dec_rs2_i
 );
+
+
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx//
+//  指令分解
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx//
+    wire [6:0] opcode = instr_i[6:0];
+    wire [4:0] rd     = instr_i[11:7];
+    wire [2:0] fun3   = instr_i[14:12];
+    wire [4:0] rs1    = instr_i[19:15];
+    wire [4:0] rs2    = instr_i[24:20];
+    wire [6:0] fun7   = instr_i[31:25];
+
+
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx//
+//  OPCODE 解析
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx//
+
+
 
 
 endmodule
--- rtl/defines.v
+++
@@ -1,5 +0,0 @@
-
-`define XLEN 32
-`define INSTR_WIDTH 32
-`define REG_IDX_WIDTH 5
-`define PC_WIDTH 32
