5cda7fb6cb4868e3cb4f8ad60d1d8d5c85f34c0a
xiachunqiudong <xiadaoliang@outlook.com>
2022-12-06 22:55:06 +0800

    2022年 12月 06日 星期二 22:55:06 CST

--- rtl/core/obj_dir/Vriscx__ALL.d
+++
@@ -1,9 +0,0 @@
-Vriscx__ALL.o: Vriscx__ALL.cpp Vriscx.cpp Vriscx.h \
- /usr/local/share/verilator/include/verilated_heavy.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilatedos.h Vriscx__Syms.h \
- Vriscx___024root.h /usr/local/share/verilator/include/verilated_vcd_c.h \
- /usr/local/share/verilator/include/verilated_trace.h \
- /usr/local/share/verilator/include/verilated_trace_defs.h \
- Vriscx___024root.cpp Vriscx__Trace.cpp Vriscx___024root__Slow.cpp \
- Vriscx__Syms.cpp Vriscx__Trace__Slow.cpp
